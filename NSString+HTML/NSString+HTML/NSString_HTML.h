f853ac6b526f742df5fed5febb3994672736c00c
Arnas Dundulis <arnas@vinted.com>
2017-01-25 16:22:31 +0200

    Add framework project to support carthage

---
+++ NSString+HTML/NSString+HTML/NSString_HTML.h
@@ -0,0 +1,18 @@
+//
+//  NSString+HTML.h
+//  NSString+HTML
+//
+//  Created by Arnas Dundulis on 1/25/17.
+//  Copyright © 2017 Koolistov Pte. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSString+HTML.
+FOUNDATION_EXPORT double NSString_HTMLVersionNumber;
+
+//! Project version string for NSString+HTML.
+FOUNDATION_EXPORT const unsigned char NSString_HTMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSString_HTML/PublicHeader.h>
+#import "NSString+HTML.h"
